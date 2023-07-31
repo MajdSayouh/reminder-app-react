6f6afddb5518a1b89be5982c96c3bfda0d946942
MajdSayouh <ali.abdo.laith@gmail.com>
2023-07-31 15:23:58 -0800

    Updates

---
+++ precache-manifest.f2d0b4caa88d20f8aad882ee2fed8df9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57caead55991e7d1a2bf01b5caf4fc0c",
+    "url": "/reminder-app-react/index.html"
+  },
+  {
+    "revision": "209d2c651c9ab55b5448",
+    "url": "/reminder-app-react/static/css/2.8589558d.chunk.css"
+  },
+  {
+    "revision": "9679070e2998f2823fb7",
+    "url": "/reminder-app-react/static/css/main.aee38e1c.chunk.css"
+  },
+  {
+    "revision": "209d2c651c9ab55b5448",
+    "url": "/reminder-app-react/static/js/2.87329218.chunk.js"
+  },
+  {
+    "revision": "9679070e2998f2823fb7",
+    "url": "/reminder-app-react/static/js/main.ec7a1bfa.chunk.js"
+  },
+  {
+    "revision": "745e5ac101bd046ed6ac",
+    "url": "/reminder-app-react/static/js/runtime~main.589db00a.js"
+  },
+  {
+    "revision": "2c51f81ea0f98a3238ca69bda8990a5f",
+    "url": "/reminder-app-react/static/media/reminder.2c51f81e.png"
+  }
+]);
